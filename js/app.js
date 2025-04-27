6a6c056bd5e8d89c4ee9c614862857b009666d85
Fabián Huertas <miloh9318@hotmail.com>
2025-04-27 11:28:42 -0500

    creando la funcion Empleados en el cual creamos el objeto donde se generaran las instancias de este

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 let empleados = [];
+
+function Empleados(legajo, nombre, apellido, nacimiento, cargo){
+    this.legajo = legajo;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.nacimiento = nacimiento;
+    this.cargo = cargo;
+}
